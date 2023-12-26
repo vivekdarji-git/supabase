ead944a314b617abf2b98deca4f3c731ab66d0e5
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-12-26 17:12:37 +0100

    Fix type errors.

--- apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.utils.ts
+++ apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/ColumnEditor/ColumnEditor.utils.ts
@@ -1,12 +1,9 @@
-import { find, isUndefined, isEqual, isNull } from 'lodash'
+import type { PostgresColumn, PostgresTable } from '@supabase/postgres-meta'
+import { find, isEqual, isNull } from 'lodash'
 import { Dictionary } from 'types'
-import type {
-  PostgresColumn,
-  PostgresRelationship,
-  PostgresTable,
-  PostgresType,
-} from '@supabase/postgres-meta'
 
+import { FOREIGN_KEY_CASCADE_ACTION } from 'data/database/database-query-constants'
+import { ForeignKeyConstraint } from 'data/database/foreign-key-constraints-query'
 import { uuidv4 } from 'lib/helpers'
 import {
   ColumnField,
@@ -14,8 +11,6 @@ import {
   ExtendedPostgresRelationship,
   UpdateColumnPayload,
 } from '../SidePanelEditor.types'
-import { FOREIGN_KEY_CASCADE_ACTION } from 'data/database/database-query-constants'
-import { ForeignKeyConstraint } from 'data/database/foreign-key-constraints-query'
 
 const isSQLExpression = (input: string) => {
   if (['CURRENT_DATE'].includes(input)) return true
@@ -90,7 +85,7 @@ export const generateCreateColumnPayload = (
   field: ColumnField
 ): CreateColumnPayload => {
   const isIdentity = field.format.includes('int') ? field.isIdentity : false
-  const defaultValue = field.defaultValue
+  const defaultValue = field.defaultValue as any
   const payload: CreateColumnPayload = {
     tableId,
     isIdentity,
@@ -126,7 +121,6 @@ export const generateUpdateColumnPayload = (
   const isOriginallyPrimaryKey = primaryKeyColumns.includes(originalColumn.name)
 
   // Only append the properties which are getting updated
-  const defaultValue = field.defaultValue
   const type = field.isArray ? `${field.format}[]` : field.format
   const comment = (field.comment?.length ?? '') === 0 ? null : field.comment
 
@@ -135,17 +129,18 @@ export const generateUpdateColumnPayload = (
     payload.name = field.name
   }
   if (!isEqual(originalColumn.comment, comment)) {
-    payload.comment = comment
+    payload.comment = comment as string | undefined
   }
   if (!isEqual(originalColumn.check?.trim(), field.check?.trim())) {
-    payload.check = field.check?.trim() || null
+    payload.check = field.check?.trim()
   }
 
   if (!isEqual(originalColumn.format, type)) {
     payload.type = type
   }
-  if (!isEqual(originalColumn.default_value as string, defaultValue)) {
-    payload.defaultValue = defaultValue
+  if (!isEqual(originalColumn.default_value, field.defaultValue)) {
+    const defaultValue = field.defaultValue
+    payload.defaultValue = defaultValue as unknown as Record<string, never> | undefined
     payload.defaultValueFormat =
       isNull(defaultValue) || isSQLExpression(defaultValue)
         ? 'expression'
