7cce6cb8894476162207ed9851f74e67c57d1edd
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-12-27 14:49:33 +0100

    Delete MetaStore.

--- apps/studio/stores/RootStore.ts
+++ apps/studio/stores/RootStore.ts
@@ -2,7 +2,6 @@ import { configure } from 'mobx'
 
 import { Project } from 'data/projects/project-detail-query'
 import UiStore, { IUiStore } from './UiStore'
-import MetaStore, { IMetaStore } from './pgmeta/MetaStore'
 
 // Temporary disable mobx warnings
 // TODO: need to remove this after refactoring old stores.
@@ -12,7 +11,6 @@ configure({
 
 export interface IRootStore {
   ui: IUiStore
-  meta: IMetaStore
 
   selectedProjectRef?: string
 
@@ -20,13 +18,11 @@ export interface IRootStore {
 }
 export class RootStore implements IRootStore {
   ui: IUiStore
-  meta: IMetaStore
 
   selectedProjectRef: string | undefined
 
   constructor() {
     this.ui = new UiStore(this)
-    this.meta = new MetaStore(this, { projectRef: '', connectionString: '' })
   }
 
   /**
@@ -41,7 +37,6 @@ export class RootStore implements IRootStore {
     // this will show the loading screen instead of showing the previous project
     this.ui.setProjectRef(undefined)
 
-    this.meta.setProjectDetails(project)
     // ui set must come last
     this.ui.setProjectRef(project.ref)
 
