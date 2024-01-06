c20d90a3714c2b92fac7e23db0f2b847ee7f99d9
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2024-01-06 12:03:10 +0100

    chore: sign impersonation JWT with browser crypto

--- apps/studio/components/interfaces/Realtime/Inspector/RealtimeTokensPopover/index.tsx
+++ apps/studio/components/interfaces/Realtime/Inspector/RealtimeTokensPopover/index.tsx
@@ -42,7 +42,9 @@ export const RealtimeTokensPopover = ({ config, onChangeConfig }: RealtimeTokens
       snap.role.type === 'postgrest'
     ) {
       token = anonServiceRoleKey
-      bearer = getRoleImpersonationJWT(config.projectRef, jwtSecret, snap.role)
+      getRoleImpersonationJWT(config.projectRef, jwtSecret, snap.role).then(
+        (token) => (bearer = token)
+      )
     } else {
       token = serviceRoleKey
     }
